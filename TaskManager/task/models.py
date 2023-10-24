d182783cb7166ca7a219bebadd859e14da546a73
Anton <firechant2@gmail.com>
2023-10-24 13:54:19 +0300

    My project

---
+++ TaskManager/task/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=255, default='')
+
+
+class Task(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    Title = models.CharField(max_length=255, default='')
+    Desc = models.TextField()
+    Dedline = models.DateTimeField()
+    Priority = models.IntegerField()
+    Status = models.CharField(max_length=255, default='')
+    Date = models.DateTimeField()
